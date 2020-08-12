34e00844f9a5ffb33a4bdbaf5f694e891f5a188c
vberzsin <vberzsin@web.de>
2020-08-12 03:11:39 +0200

    setup.sh at named db pipe

--- db/init.sh
+++ db/init.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-mysqld
-mysql -u $MYSQL_CLIENT -p$MYSQL_ROOT_PASSWORD -e "grant all on *.* to 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD';"
-mysql -u $MYSQL_CLIENT -p$MYSQL_ROOT_PASSWORD -e "create database $MYSQL_DATABASE;"
-mysql -u $MYSQL_CLIENT -p$MYSQL_ROOT_PASSWORD -e "grant all on $MYSQL_DATABASE.* to 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD';"
+echo "$MYSQL_CLIENT"
+echo "$MYSQL_ROOT_PASSWORD"
+
+mysql -u$MYSQL_CLIENT -p$MYSQL_ROOT_PASSWORD -e "grant all on *.* to 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD';"
+mysql -u$MYSQL_CLIENT -p$MYSQL_ROOT_PASSWORD -e "grant all on *.* to 'root'@'127.0.0.1' identified by '$MYSQL_ROOT_PASSWORD';"
+mysql -u$MYSQL_CLIENT -p$MYSQL_ROOT_PASSWORD -e "grant all on *.* to 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';"
+mysql -u$MYSQL_CLIENT -p$MYSQL_ROOT_PASSWORD -e "flush privileges"
+mysql -u$MYSQL_CLIENT -p$MYSQL_ROOT_PASSWORD -e "create database $MYSQL_DATABASE;"
+mysql -u$MYSQL_CLIENT -p$MYSQL_ROOT_PASSWORD -e "grant all on $MYSQL_DATABASE.* to 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD';"
+mysql -u$MYSQL_CLIENT -p$MYSQL_ROOT_PASSWORD -e "grant all on $MYSQL_DATABASE.* to 'root'@'127.0.0.1' identified by '$MYSQL_ROOT_PASSWORD';"
+
--- mage/magento2/setup.sh
+++ mage/magento2/setup.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-bin/magento setup:install \
-	--base-url=http://127.0.0.1 \
-	--db-host=db \
-	--db-name=magento20001 \
-	--db-user=magento20001 \
-	--db-password=magento20001 \
-	--admin-firstname=magento \
-	--admin-lastname=client \
+bin/magento setup:install 		\
+	--base-url=http://127.0.0.1 	\
+	--db-host=db 			\
+	--db-name=magento20001 		\
+	--db-user=magento20001 		\
+	--db-user=root			\
+	--db-password=magento20001 	\
+	--admin-firstname=magento 	\
+	--admin-lastname=client 	\
 	--admin-email=client@magento.local.com \
-	--admin-user=admin123 \
-	--admin-password=admin123 \
-	--language=en_US \
-	--currency=USD \
-	--timezone=America/Chicago \
+	--admin-user=admin123 		\
+	--admin-password=admin123 	\
+	--language=en_US 		\
+	--currency=USD 			\
+	--timezone=America/Chicago 	\
 	--use-rewrites=1
 
 
